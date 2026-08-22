c648d659e5308525484f49df903aa3a894143241
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-23 02:04:55 +0300

    Add Prompt class to Basefy.Data.Models
    Introduced the Prompt class with Id, llmModel, and versions properties. Included necessary using directives for related namespaces.

---
+++ Basefy.Data/Models/Prompt.cs
@@ -0,0 +1,17 @@
+﻿using Basefy.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basefy.Data.Models
+{
+    public class Prompt
+    {
+        public int Id { get; set; }
+        public LlmModel llmModel { get; set; }
+        public List<PromptVersion> versions { get; set; }
+
+    }
+}
