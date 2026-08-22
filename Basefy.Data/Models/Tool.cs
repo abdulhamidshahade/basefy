e97ec9872c35b5d048a0007e0c2ff92bc56b11e3
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-23 02:15:50 +0300

    Add Tool class to Basefy.Data.Models
    A new Tool class was introduced with properties for Id, Name, PromptVersionId, a navigation property to PromptVersion, and a collection of ToolVersion objects. Added necessary using directives for collections and threading.

---
+++ Basefy.Data/Models/Tool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basefy.Data.Models
+{
+    public class Tool
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PromptVersionId { get; set; }
+        public PromptVersion PromptVersion { get; set; }
+        public List<ToolVersion> ToolVersions { get; set; }
+    }
+}
