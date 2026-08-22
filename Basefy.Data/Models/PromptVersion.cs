4716a84d6486cd85e7b2da3dd0c3b537e011c0e0
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-23 02:05:09 +0300

    Add PromptVersion class with Id and Prompt properties
    Introduced PromptVersion class in Basefy.Data.Models with Id and Prompt properties. Included necessary .NET using directives.

---
+++ Basefy.Data/Models/PromptVersion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basefy.Data.Models
+{
+    public class PromptVersion
+    {
+        public int Id { get; set; }
+        public Prompt prompt { get; set; }
+    }
+}
