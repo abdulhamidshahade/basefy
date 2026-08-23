a582f3913b2bd3cc8883866420f88614dc63c964
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-23 15:09:20 +0300

    Add Prompt class with Id, llmModel, and versions
    Introduced Prompt class in Basefy.Domain.Models with properties for Id, llmModel (LlmModel), and versions (List<PromptVersion>). Imported Basefy.Domain.Data.

---
+++ Basefy.Domain/Models/Prompt.cs
@@ -0,0 +1,11 @@
+﻿using Basefy.Domain.Data;
+
+namespace Basefy.Domain.Models
+{
+    public class Prompt
+    {
+        public int Id { get; set; }
+        public LlmModel llmModel { get; set; }
+        public List<PromptVersion> versions { get; set; }
+    }
+}
