9d2fe94b41ef56f4338930df1132492e5738558b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-23 15:08:15 +0300

    Remove Basefy.Data project and all data model files
    All source files in the Basefy.Data project have been deleted, including Basefy.Data.csproj and all related C# class files (LlmModel.cs, Prompt.cs, PromptVersion.cs, Tenant.cs, Tool.cs, ToolVersion.cs). This removes the entire Basefy.Data project and its data model definitions.

--- Basefy.Data/Models/Tenant.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Basefy.Data.Models
-{
-    public class Tenant
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Prompt> prompts { get; set; }
-    }
-}
