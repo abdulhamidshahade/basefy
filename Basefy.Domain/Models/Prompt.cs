2051f476cfc505ab40afd6e171730644bcf55edd
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-23 22:26:09 +0300

    Add using directives and new properties to Prompt class
    Added System, System.Collections.Generic, System.Linq, System.Text, and System.Threading.Tasks using directives. Introduced TenantId, Title, and Content properties to the Prompt class.

--- Basefy.Domain/Models/Prompt.cs
+++ Basefy.Domain/Models/Prompt.cs
@@ -1,10 +1,19 @@
 ﻿using Basefy.Domain.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Basefy.Domain.Models
 {
     public class Prompt
     {
         public int Id { get; set; }
+        public int TenantId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        
         public LlmModel llmModel { get; set; }
         public List<PromptVersion> versions { get; set; }
     }
