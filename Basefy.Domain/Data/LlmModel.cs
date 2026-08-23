9c3c0db480f3078e58c0d9b87ff4bb83c01af8ed
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-23 15:11:04 +0300

    Add LlmModel enum with GPT and Gemma values
    Defined LlmModel enum in Basefy.Domain.Data with values: gpt_4_o, gpt_5_1, and gemma_4_26_b. Included standard .NET using directives.

---
+++ Basefy.Domain/Data/LlmModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basefy.Domain.Data
+{
+    public enum LlmModel
+    {
+        gpt_4_o,
+        gpt_5_1,
+        gemma_4_26_b
+    }
+}
