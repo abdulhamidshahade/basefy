ccfae48046dfde6227f84b1c06a6fed04696b3a2
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-24 23:41:29 +0300

    Add ToolVersion class to domain models
    Added ToolVersion class in Basefy.Domain.Models with Id, ToolId, Title, Content properties, and a Tool navigation property.

---
+++ Basefy.Domain/Models/ToolVersion.cs
@@ -0,0 +1,11 @@
+﻿namespace Basefy.Domain.Models
+{
+    public class ToolVersion
+    {
+        public int Id { get; set; }
+        public int ToolId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Tool Tool { get; set; }
+    }
+}
