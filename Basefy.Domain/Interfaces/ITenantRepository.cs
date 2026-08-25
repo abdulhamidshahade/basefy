9008291e248c8c1758465b04e15bc51dd8ee3eda
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-25 23:35:46 +0300

    Add ITenantRepository interface
    Introduced ITenantRepository in Basefy.Domain.Interfaces.
    Defines async methods for CRUD operations on Tenant entity.

---
+++ Basefy.Domain/Interfaces/ITenantRepository.cs
@@ -0,0 +1,17 @@
+﻿using Basefy.Domain.Models;
+
+namespace Basefy.Domain.Interfaces
+{
+    public interface ITenantRepository
+    {
+        Task<Tenant?> GetByIdAsync(int id);
+
+        Task<IReadOnlyList<Tenant>> GetAllAsync();
+
+        Task<int> CreateAsync(Tenant tenant);
+
+        Task<bool> UpdateAsync(Tenant tenant);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
