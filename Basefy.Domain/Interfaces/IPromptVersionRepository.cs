ec4d94f30d9167c414e54fa3bedb187669387ba2
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-25 23:35:19 +0300

    Add IPromptVersionRepository interface for PromptVersion
    Introduced IPromptVersionRepository in Basefy.Domain.Interfaces to define async CRUD operations for PromptVersion entities, including methods for get by ID, get all, get by prompt ID, create, update, and delete.

---
+++ Basefy.Domain/Interfaces/IPromptVersionRepository.cs
@@ -0,0 +1,19 @@
+﻿using Basefy.Domain.Models;
+
+namespace Basefy.Domain.Interfaces
+{
+    public interface IPromptVersionRepository
+    {
+        Task<PromptVersion?> GetByIdAsync(int id);
+
+        Task<IReadOnlyList<PromptVersion>> GetAllAsync();
+
+        Task<IReadOnlyList<PromptVersion>> GetByPromptIdAsync(int promptId);
+
+        Task<int> CreateAsync(PromptVersion promptVersion);
+
+        Task<bool> UpdateAsync(PromptVersion promptVersion);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
